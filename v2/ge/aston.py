fa5b47b2858513929102384a3ab8c2624485af52
walkerKemp <36144194+walkerKemp@users.noreply.github.com>
2024-08-25 03:09:38 -0400

    adding more node structures! 2 node layers deep

--- v2/ge/aston.py
+++ v2/ge/aston.py
@@ -1,37 +1,46 @@
 from raylibpy import *
-from ge.engine_node import EngineNode, AnimationNode
+from ge.engine_node import EngineNode, AnimationNode, PlayerNode
 
 def run_game():
     window_data = {
         "width": 1920,
         "height": 1080,
         "title": "Isometric",
-        "target-fps": 144
+        "target-fps": 144,
+        "debug": False
     }
 
     init_window(
         window_data["width"],
         window_data["height"],
-        window_data["title"]
+        window_data["title"],
     )
 
     set_config_flags(FLAG_VSYNC_HINT)
     toggle_fullscreen()
 
+    global_camera = Camera2D(Vector2(), Vector2(), 0.0, 1.0)
+
+
     root_node = EngineNode(window_data, None)
+    player_node = PlayerNode(window_data, root_node, position=Vector2(400, 400))
     animation_node = AnimationNode(window_data, root_node, "assets/idle")
-    root_node.children.append(animation_node)
+    animation_node.lock_node_position(player_node)
+
+    root_node.add_node(player_node.add_node(animation_node))
 
     while not window_should_close():
         frame_time = get_frame_time()
         root_node._on_update(frame_time)
 
         begin_drawing()
+        begin_mode2d(global_camera)
         clear_background(WHITE)
 
         root_node._on_render()
 
         draw_fps(4, 4)
+        end_mode2d()
         end_drawing()
 
     close_window()
--- v2/ge/engine_node.py
+++ v2/ge/engine_node.py
@@ -1,16 +1,26 @@
-from typing import Optional, List
+from typing import Optional, List, Dict
 from raylibpy import *
 import os
 
-class GameState:
-    pass
-
 class EngineNode:
     # if parent is none than this is the root object, all children will have a parent
-    def __init__(self, game_state: 'GameState', parent: Optional['EngineNode'], *_args, **_kwargs):
+    def __init__(self, game_state: dict[str, any], parent: Optional['EngineNode'], *_args, **_kwargs):
         self.parent: Optional['EngineNode'] = parent
-        self.children: List['EngineNode'] = []
-        self.game_state: 'GameState' = game_state
+        self.children: Dict['EngineNode'] = {}
+        self._children_pointer = 0
+        self.game_state  = game_state
+
+        self.position: Vector2 = _kwargs.get("position", Vector2())
+        self.velocity: Vector2 = _kwargs.get("velocity", Vector2())
+
+        self.default_physics = False
+        self._position_lock_target: Optional['EngineNode'] = None
+
+    def add_node(self, node: 'EngineNode') -> 'EngineNode':
+        self.children[self._children_pointer] = node
+        self._children_pointer += 1
+        node.parent = self
+        return self
 
     def on_render_ui(self):
         return None
@@ -27,34 +37,70 @@ class EngineNode:
     def _on_render(self):
         self.on_render()
         
-        for child in self.children:
+        for _id, child in self.children.items():
             child._on_render()
 
     def _on_update(self, delta_time: float):
         self.on_update(delta_time)
 
-        for child in self.children:
+        if self.default_physics:
+            self.position += self.velocity * delta_time
+
+        if self._position_lock_target != None:
+            self.position = self._position_lock_target.position
+
+        for _id, child in self.children.items():
             child._on_update(delta_time)
 
+    def lock_node_position(self, other: 'EngineNode'):
+        self._position_lock_target = other
+
 class PlayerNode(EngineNode):
-    pass
+    def __init__(self, game_state: dict['str', 'str'], parent: Optional['EngineNode'], *_args, **_kwargs):
+        super().__init__(game_state, parent, *_args, **_kwargs)
 
 class AnimationNode(EngineNode):
-    def __init__(self, game_state: 'GameState', parent: Optional['EngineNode'], animation_folder: str):
-        super().__init__(game_state, parent)
+    def __init__(self, game_state: dict['str', 'str'], parent: Optional['EngineNode'], animation_folder: str, *_args, **_kwargs):
+        super().__init__(game_state, parent, *_args, **_kwargs)
 
         self.key_frames = []
         for file in os.listdir(animation_folder):
             self.key_frames.append(load_texture(os.path.join(animation_folder, file)))
 
+        
         self.frame_index = 0
         self.frame_index_max = len(self.key_frames)
+
+        assert(self.frame_index_max > 0, "test")
+
+        if game_state["debug"]:
+            print(f"Animation loaded with {self.frame_index_max} frames.")
 
         self.frame_rate = 1.0 / 3.0
         self.frame_time = 0.0
 
+        self.width = self.key_frames[0].width
+        self.height = self.key_frames[1].height
+        self.scale = 1
+
     def on_render(self):
-        draw_texture(self.key_frames[self.frame_index], 32, 32, WHITE)
+        centered_position = Vector2(
+            self.position.x - self.width / 2,
+            self.position.y - self.height / 2
+        )
+
+        key_frame = self.key_frames[self.frame_index]
+
+        draw_texture_ex(
+            key_frame,
+            centered_position,
+            0,
+            self.scale,
+            WHITE
+        )
+
+        if self.game_state["debug"]:
+            draw_circle(self.position.x, self.position.y, 2, GREEN)
 
     def on_update(self, delta_time: float):
         self.frame_time += delta_time
